81e7127c383546671c4dee968803579ac76af6e5
Abraham Mengistu <abraham.a.mengistu@gmail.com>
2023-07-07 09:33:34 -0400

    Update code for addition along with handling input/output

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # Design and implement a simple calculator program in Python that takes user input for two numbers 
 # and performs basic arithmetic operations (addition, subtraction, multiplication, division). 
 # The program should provide a user-friendly interface and handle invalid inputs gracefully.
+import sys
+
+exit_program = False # flag variable
+
+def set_exit_program():
+    global exit_program
+    exit_program = True # prevents the program from restarting from the beginning
+    sys.exit()  # Exit the program
 
 def main():
     # ask user what do they want to do (add, subtract, multiply, or divide)
     # ask user for the numbers that they want to operate on
         # handle invalid numbers with exceptions based on operation
     # return result of operation to the user
+
+    # global exit_program # we need to declare the variable as global inside the main() function
     while True:
         operation = str(input("""Select the mathematical operation you want to do:
 -- Addition
@@ -18,7 +28,37 @@ Type "exit" to stop this program.
 """))
 
         if operation.lower() == "addition":
-            pass
+            # validation for first number
+            while True:
+                num1 = input("First number: ")
+                if num1.lower() == "exit":
+                    set_exit_program()
+                try:
+                    num1 = float(num1)
+                    break
+                except ValueError:
+                    print("You must type in a number.")
+                except Exception as e:
+                    print(f"An error occured: {str(e)}")
+            # validation for second number
+            while True:
+                num2 = input("Second number: ")
+                if num2.lower() == "exit":
+                    set_exit_program()
+                try:
+                    num2 = float(num2)
+                    break
+                except ValueError:
+                    print("You must type in a number.")
+                except Exception as e:
+                    print(f"An error occured: {str(e)}")
+            result = num1 + num2
+            # dispay the result as int if it ends in .0
+            if result.is_integer():
+                formatted_result = int(result)
+            else:
+                formatted_result = result
+            print(formatted_result)
         elif operation.lower() == "subraction":
             pass
         elif operation.lower() == "multiplication":
