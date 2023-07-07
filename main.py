93842a80863c9b5cbdc022bd126c33e8096901e7
Abraham Mengistu <abraham.a.mengistu@gmail.com>
2023-07-06 21:46:39 -0400

    Create main function

---
+++ main.py
@@ -0,0 +1,9 @@
+# Design and implement a simple calculator program in Python that takes user input for two numbers 
+# and performs basic arithmetic operations (addition, subtraction, multiplication, division). 
+# The program should provide a user-friendly interface and handle invalid inputs gracefully.
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
